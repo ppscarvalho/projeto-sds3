85d003191a922be181480771ab203de3295497dc
Pedro Carvalho <ppscarvalho@gmail.com>
2021-05-08 18:32:09 -0300

    First request => Nao consegui terminar essa tarefa
    por erro na linha const data = response.data as SaleSum[]
    
    a palavra as nao esta sendo reconhecida

--- frontend/src/components/DonutChart/index.jsx
+++ frontend/src/components/DonutChart/index.jsx
@@ -1,6 +1,9 @@
 import Chart from 'react-apexcharts';
 
 const DonutChart = () => {
+  ///FORMA ERRADA
+  let chartData: ChartData = { labels: [], series: [] };
+
   const mockData = {
     series: [477138, 499928, 444867, 220426, 473088],
     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé'],
